1d681947d69265a67386771e5ade1c15c87e29e8
Charilaos Memeletzoglou <cmemeletzoglou@antmicro.com>
2026-07-27 16:18:01 +0200

    Added XLEN support on TB and related files
    Signed-off-by: Charilaos Memeletzoglou <cmemeletzoglou@antmicro.com>

--- testbench/ahb_sif.sv
+++ testbench/ahb_sif.sv
@@ -16,7 +16,7 @@
 `ifdef RV_BUILD_AHB_LITE
 
 module ahb_sif (
-input logic [63:0] HWDATA,
+input logic [`RV_BUS_WIDTH-1:0] HWDATA,
 input logic HCLK,
 input logic HSEL,
 input logic [3:0] HPROT,
@@ -25,22 +25,23 @@ input logic [1:0] HTRANS,
 input logic [2:0] HSIZE,
 input logic HREADY,
 input logic HRESETn,
-input logic [31:0] HADDR,
+input logic [`RV_XLEN-1:0] HADDR,
 input logic [2:0] HBURST,
 
 output logic HREADYOUT,
 output logic HRESP,
-output logic [63:0] HRDATA
+output logic [`RV_BUS_WIDTH-1:0] HRDATA
 );
 
-parameter MAILBOX_ADDR = 32'hD0580000;
+localparam int ADDR_BUS_LSB_OFFSET = $clog2(`RV_BUS_BYTE_WIDTH);
+parameter MAILBOX_ADDR = {{(`RV_XLEN-32){1'b0}}, 32'hD0580000};
 
 logic write;
-logic [31:0] laddr, addr;
-logic [7:0] strb_lat;
-logic [63:0] rdata;
+logic [`RV_XLEN-1:0] laddr, addr;
+logic [`RV_BUS_BYTE_WIDTH-1:0] strb_lat;
+logic [`RV_BUS_WIDTH-1:0] rdata;
 
-bit [7:0] mem [bit[31:0]];
+bit [7:0] mem [bit[`RV_XLEN-1:0]];
 bit [7:0] wscnt;
 int dws = 0;
 int iws = 0;
@@ -49,11 +50,12 @@ bit iws_rand;
 bit ok;
 
 // Wires
-wire [63:0] WriteData = HWDATA;
-wire [7:0] strb =  HSIZE == 3'b000 ? 8'h1 << HADDR[2:0] :
-                   HSIZE == 3'b001 ? 8'h3 << {HADDR[2:1],1'b0} :
-                   HSIZE == 3'b010 ? 8'hf << {HADDR[2],2'b0} : 8'hff;
-
+wire [`RV_BUS_WIDTH-1:0] WriteData = HWDATA;
+wire [`RV_BUS_BYTE_WIDTH-1:0] strb =    HSIZE == 3'b000 ? `RV_BUS_BYTE_WIDTH'('h1) << HADDR[ADDR_BUS_LSB_OFFSET-1:0] :
+                                        HSIZE == 3'b001 ? `RV_BUS_BYTE_WIDTH'('h3) << {HADDR[ADDR_BUS_LSB_OFFSET-1:1],1'b0} :
+                                        HSIZE == 3'b010 ? `RV_BUS_BYTE_WIDTH'('hf) << {HADDR[ADDR_BUS_LSB_OFFSET-1:2],2'b0} :
+                                        HSIZE == 3'b011 ? (`RV_BUS_WIDTH == 64 ? `RV_BUS_BYTE_WIDTH'('hff) : `RV_BUS_BYTE_WIDTH'('hff) << {HADDR[ADDR_BUS_LSB_OFFSET-1], 3'b0}) :
+                                        HSIZE == 3'b100 & `RV_BUS_WIDTH == 128 ? `RV_BUS_BYTE_WIDTH'('hffff) : 0;
 
 wire mailbox_write = write && HSEL && HREADY && laddr==MAILBOX_ADDR;
 
@@ -65,36 +67,28 @@ initial begin
     iws_rand = iws < 0;
 end
 
-
-
 always @ (negedge HCLK ) begin
     if(HREADY)
         addr = HADDR;
     if (write & HREADY) begin
-        if(strb_lat[7]) mem[{laddr[31:3],3'd7}] = HWDATA[63:56];
-        if(strb_lat[6]) mem[{laddr[31:3],3'd6}] = HWDATA[55:48];
-        if(strb_lat[5]) mem[{laddr[31:3],3'd5}] = HWDATA[47:40];
-        if(strb_lat[4]) mem[{laddr[31:3],3'd4}] = HWDATA[39:32];
-        if(strb_lat[3]) mem[{laddr[31:3],3'd3}] = HWDATA[31:24];
-        if(strb_lat[2]) mem[{laddr[31:3],3'd2}] = HWDATA[23:16];
-        if(strb_lat[1]) mem[{laddr[31:3],3'd1}] = HWDATA[15:08];
-        if(strb_lat[0]) mem[{laddr[31:3],3'd0}] = HWDATA[07:00];
+        for (int i=0; i<`RV_BUS_BYTE_WIDTH; i++) begin
+            if (strb_lat[i]) mem[{laddr[`RV_XLEN-1:3], 3'(i)}] = HWDATA[i*8 +: 8];
+        end
     end
     if(HREADY & HSEL & |HTRANS) begin
-`ifdef VERILATOR
+    `ifdef VERILATOR
         if(iws_rand & ~HPROT[0])
             iws = $random & 15;
         if(dws_rand & HPROT[0])
             dws = $random & 15;
-`else
+    `else
         if(iws_rand & ~HPROT[0])
             ok = std::randomize(iws) with {iws dist {0:=10, [1:3]:/2, [4:15]:/1};};
         if(dws_rand & HPROT[0])
             ok = std::randomize(dws) with {dws dist {0:=10, [1:3]:/2, [4:15]:/1};};
-`endif
+    `endif
     end
 end
-
 
 assign HRDATA = HREADY ? rdata : ~rdata;
 assign HREADYOUT = wscnt == 0;
@@ -102,7 +96,7 @@ assign HRESP = 0;
 
 always @(posedge HCLK or negedge HRESETn) begin
     if(~HRESETn) begin
-        laddr <= 32'b0;
+        laddr <= {`RV_XLEN{1'b0}};
         write <= 1'b0;
         rdata <= '0;
         wscnt <= 0;
@@ -111,15 +105,11 @@ always @(posedge HCLK or negedge HRESETn) begin
         if(HREADY & HSEL) begin
             laddr <= HADDR;
             write <= HWRITE & |HTRANS;
-            if(|HTRANS & ~HWRITE)
-                rdata <= {mem[{addr[31:3],3'd7}],
-                          mem[{addr[31:3],3'd6}],
-                          mem[{addr[31:3],3'd5}],
-                          mem[{addr[31:3],3'd4}],
-                          mem[{addr[31:3],3'd3}],
-                          mem[{addr[31:3],3'd2}],
-                          mem[{addr[31:3],3'd1}],
-                          mem[{addr[31:3],3'd0}]};
+            if(|HTRANS & ~HWRITE) begin
+                for (int i=0; i<`RV_BUS_BYTE_WIDTH; i++) begin
+                    rdata[i*8 +: 8] = mem[{addr[`RV_XLEN-1:3], 3'(i)}];
+                end
+            end
             strb_lat <= strb;
         end
     end
@@ -129,57 +119,57 @@ always @(posedge HCLK or negedge HRESETn) begin
         wscnt <= wscnt-1;
 end
 
-
 endmodule
 `endif
 
 `ifdef RV_BUILD_AXI4
 module axi_slv #(TAGW=1) (
-input                   aclk,
-input                   rst_l,
-input                   arvalid,
-output reg              arready,
-input [31:0]            araddr,
-input [TAGW-1:0]        arid,
-input [7:0]             arlen,
-input [1:0]             arburst,
-input [2:0]             arsize,
+input                           aclk,
+input                           rst_l,
+input                           arvalid,
+output reg                      arready,
+input [`RV_XLEN-1:0]            araddr,
+input [TAGW-1:0]                arid,
+input [7:0]                     arlen,
+input [1:0]                     arburst,
+input [2:0]                     arsize,
 
-output reg              rvalid,
-input                   rready,
-output reg [63:0]       rdata,
-output reg [1:0]        rresp,
-output reg [TAGW-1:0]   rid,
-output                  rlast,
+output reg                      rvalid,
+input                           rready,
+output reg [`RV_BUS_WIDTH-1:0]  rdata,
+output reg [1:0]                rresp,
+output reg [TAGW-1:0]           rid,
+output                          rlast,
 
-input                   awvalid,
-output                  awready,
-input [31:0]            awaddr,
-input [TAGW-1:0]        awid,
-input [7:0]             awlen,
-input [1:0]             awburst,
-input [2:0]             awsize,
+input                           awvalid,
+output                          awready,
+input [`RV_XLEN-1:0]            awaddr,
+input [TAGW-1:0]                awid,
+input [7:0]                     awlen,
+input [1:0]                     awburst,
+input [2:0]                     awsize,
 
-input [63:0]            wdata,
-input [7:0]             wstrb,
-input                   wvalid,
-output                  wready,
+input [`RV_BUS_WIDTH-1:0]       wdata,
+input [`RV_BUS_BYTE_WIDTH-1:0]  wstrb,
+input                           wvalid,
+output                          wready,
 
-output  reg             bvalid,
-input                   bready,
-output reg [1:0]        bresp,
-output reg [TAGW-1:0]   bid
+output  reg                     bvalid,
+input                           bready,
+output reg [1:0]                bresp,
+output reg [TAGW-1:0]           bid
 );
 
-parameter MAILBOX_ADDR = 32'hD0580000;
+localparam int ADDR_BUS_LSB_OFFSET = $clog2(`RV_BUS_BYTE_WIDTH);
+parameter MAILBOX_ADDR = {{(`RV_XLEN-32){1'b0}},32'hD0580000};
 parameter MEM_SIZE_DW = 8192;
 
-bit [7:0] mem [bit[31:0]];
-bit [63:0] memdata;
-wire [63:0] WriteData;
+bit [7:0] mem [bit[`RV_XLEN-1:0]];
+bit [`RV_BUS_WIDTH-1:0] memdata;
+wire [`RV_BUS_WIDTH-1:0] WriteData;
 wire mailbox_write;
 
-wire[31:0] raddr, waddr;
+wire[`RV_XLEN-1:0] raddr, waddr;
 
 assign mailbox_write = awvalid && awaddr==MAILBOX_ADDR && rst_l;
 assign WriteData = wdata;
@@ -198,24 +188,22 @@ always @ ( posedge aclk or negedge rst_l) begin
     end
 end
 
-assign raddr = {araddr[31:3],3'b0};
-assign waddr = {awaddr[31:3],3'b0};
+assign raddr = {araddr[`RV_XLEN-1:ADDR_BUS_LSB_OFFSET], {ADDR_BUS_LSB_OFFSET{1'b0}}};
+assign waddr = {awaddr[`RV_XLEN-1:ADDR_BUS_LSB_OFFSET], {ADDR_BUS_LSB_OFFSET{1'b0}}};
 
 always @ ( negedge aclk) begin
-    if(arvalid) memdata <= {mem[raddr+7], mem[raddr+6], mem[raddr+5], mem[raddr+4],
-                            mem[raddr+3], mem[raddr+2], mem[raddr+1], mem[raddr]};
+    if(arvalid) begin
+        for (int i=0; i<`RV_BUS_BYTE_WIDTH; i++) begin
+            memdata[i*8 +: 8] = mem[raddr+i];
+        end
+    end
+
     if(awvalid) begin
-        if(wstrb[7]) mem[waddr+7] = wdata[63:56];
-        if(wstrb[6]) mem[waddr+6] = wdata[55:48];
-        if(wstrb[5]) mem[waddr+5] = wdata[47:40];
-        if(wstrb[4]) mem[waddr+4] = wdata[39:32];
-        if(wstrb[3]) mem[waddr+3] = wdata[31:24];
-        if(wstrb[2]) mem[waddr+2] = wdata[23:16];
-        if(wstrb[1]) mem[waddr+1] = wdata[15:08];
-        if(wstrb[0]) mem[waddr+0] = wdata[07:00];
+        for (int i=0; i<`RV_BUS_BYTE_WIDTH; i++) begin
+            if(wstrb[i]) mem[waddr+i] = wdata[i*8 +: 8];
+        end
     end
 end
-
 
 assign arready = 1'b1;
 assign awready = 1'b1;

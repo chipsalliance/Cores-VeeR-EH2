ad19b27f423be649310e05f7b6fbd7047ee59f43
Robert Szczepanski <rszczepanski@antmicro.com>
2026-08-06 11:19:59 +0200

    rtl: beh_lib: Add proper secded for 64-bit ECC

--- design/lib/beh_lib.sv
+++ design/lib/beh_lib.sv
@@ -1168,7 +1168,7 @@ endmodule // rvecc_decode
 
 module rvecc_encode_64  (
                       input [63:0] din,
-                      output [6:0] ecc_out
+                      output [7:0] ecc_out
                       );
   assign ecc_out[0] = din[0]^din[1]^din[3]^din[4]^din[6]^din[8]^din[10]^din[11]^din[13]^din[15]^din[17]^din[19]^din[21]^din[23]^din[25]^din[26]^din[28]^din[30]^din[32]^din[34]^din[36]^din[38]^din[40]^din[42]^din[44]^din[46]^din[48]^din[50]^din[52]^din[54]^din[56]^din[57]^din[59]^din[61]^din[63];
 
@@ -1184,17 +1184,25 @@ module rvecc_encode_64  (
 
    assign ecc_out[6] = din[57]^din[58]^din[59]^din[60]^din[61]^din[62]^din[63];
 
+   assign ecc_out[7] = (^din[63:0] ^ (^ecc_out[6:0]));
+
 endmodule // rvecc_encode_64
 
 
 module rvecc_decode_64  (
                       input         en,
                       input [63:0]  din,
-                      input [6:0]   ecc_in,
-                      output        ecc_error
+                      input [7:0]   ecc_in,
+                      input         sed_ded,
+                      output [63:0] dout,
+                      output [7:0]  ecc_out,
+                      output        single_ecc_error,
+                      output        double_ecc_error
                       );
 
-   logic [6:0]                      ecc_check;
+   logic [7:0]  ecc_check;
+   logic [71:0] error_mask;
+   logic [71:0] din_plus_parity, dout_plus_parity;
 
    // Generate the ecc bits
    assign ecc_check[0] = ecc_in[0]^din[0]^din[1]^din[3]^din[4]^din[6]^din[8]^din[10]^din[11]^din[13]^din[15]^din[17]^din[19]^din[21]^din[23]^din[25]^din[26]^din[28]^din[30]^din[32]^din[34]^din[36]^din[38]^din[40]^din[42]^din[44]^din[46]^din[48]^din[50]^din[52]^din[54]^din[56]^din[57]^din[59]^din[61]^din[63];
@@ -1211,7 +1219,23 @@ module rvecc_decode_64  (
 
    assign ecc_check[6] = ecc_in[6]^din[57]^din[58]^din[59]^din[60]^din[61]^din[62]^din[63];
 
-   assign ecc_error = en & (ecc_check[6:0] != 0);  // all errors in the sed_ded case will be recorded as DE
+   // This is the parity bit
+   assign ecc_check[7] = ((^din[63:0])^(^ecc_in[7:0])) & ~sed_ded;
+
+   assign single_ecc_error = en & (ecc_check[7:0] != 0) & ecc_check[7];   // this will never be on for sed_ded
+   assign double_ecc_error = en & (ecc_check[7:0] != 0) & ~ecc_check[7];  // all errors in the sed_ded case will be recorded as DE
+
+   // Generate the mask for error correcting
+   for (genvar i=1; i<73; i++) begin
+      assign error_mask[i-1] = (ecc_check[6:0] == i);
+   end
+
+   // Generate the corrected data
+   assign din_plus_parity[71:0] = {ecc_in[7], din[63:57], ecc_in[6], din[56:26], ecc_in[5], din[25:11], ecc_in[4], din[10:4], ecc_in[3], din[3:1], ecc_in[2], din[0], ecc_in[1:0]};
+
+   assign dout_plus_parity[71:0] = single_ecc_error ? (error_mask[71:0] ^ din_plus_parity[71:0]) : din_plus_parity[71:0];
+   assign dout[63:0]             = {dout_plus_parity[70:64], dout_plus_parity[62:32], dout_plus_parity[30:16], dout_plus_parity[14:8], dout_plus_parity[6:4], dout_plus_parity[2]};
+   assign ecc_out[7:0]           = {(dout_plus_parity[71] ^ (ecc_check[7:0] == 8'b10000000)), dout_plus_parity[63], dout_plus_parity[31], dout_plus_parity[15], dout_plus_parity[7], dout_plus_parity[3], dout_plus_parity[1:0]};
 
  endmodule // rvecc_decode_64
 
